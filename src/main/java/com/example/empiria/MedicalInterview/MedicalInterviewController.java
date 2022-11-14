89d6de6e465e53c940b09ee043663fa2cf069d68
KubaUli <ulikowskij@gmail.com>
2022-11-14 16:42:26 +0100

    v4
    I added logicm that prevents the addition of two people, the way to add an address, and the input of the first form

---
+++ src/main/java/com/example/empiria/MedicalInterview/MedicalInterviewController.java
@@ -0,0 +1,38 @@
+package com.example.empiria.MedicalInterview;
+
+import com.example.empiria.PersonAddres.PersonAddresDto;
+import com.example.empiria.PersonAddres.PersonAddresService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.validation.Valid;
+
+@Controller
+public class MedicalInterviewController {
+    private final MedicalInterviewService medicalInterviewService;
+
+    public MedicalInterviewController(MedicalInterviewService medicalInterviewService) {
+        this.medicalInterviewService = medicalInterviewService;
+    }
+
+    @GetMapping("/MedicalInterview")
+    String addUser(Model model) {
+        model.addAttribute("personAddres", new MedicalInterviewDto());
+        return "addUser";
+    }
+
+    @PostMapping("/InterviewADD")
+    String registerUser(@Valid @ModelAttribute("interview") MedicalInterviewDto medicalInterviewDto,
+                        BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+                return "MedicalInterview";
+        } else {
+            medicalInterviewService.registerInterview(medicalInterviewDto);
+           return "redirect:success";
+        }
+    }
+}
